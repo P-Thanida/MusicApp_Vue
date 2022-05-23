59485612ca8a4908e5f488c2a79e1db6fc51a78b
P-Thanida <s6135512015@phuket.psu.ac.th>
2022-05-23 11:06:50 +0700

    add button

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,70 @@
     </header>
     <main>
       <section class="player">
-        <h2 class="song-title">Test</h2>
+        <h2 class="song-title">
+          {{ current.title }} - <span>{{ current.artist }}</span>
+        </h2>
+        <span>
+          <div class="controls">
+            <button class="prev">Prev</button>
+            <button class="play">Play</button>
+            <button class="pause">Pause</button>
+            <button class="next">Next</button>
+          </div>
+        </span>
       </section>
     </main>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
- 
-}
+  name: 'app',
+  data() {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [
+        {
+          title: 'Jak kan duai dee',
+          artist: 'Tilly Birds',
+          src: require('./assets/s1.mp3'),
+        },
+        {
+          title: 'Dai tae nuk tung',
+          artist: 'Only Monday',
+          src: require('./assets/s2.mp3'),
+        },
+      ],
+      player: new Audio(),
+    };
+  },
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    // this.player.play();
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-body{
+body {
   font-family: sans-serif;
 }
 
-header{
+header {
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 15px;
   background-color: #212121;
-  color: #FFF;
-  
+  color: #fff;
 }
 </style>
